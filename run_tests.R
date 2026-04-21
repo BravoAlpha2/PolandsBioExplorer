7f25684fde982222901c3621c322094eb02b2ed2
Nuno Garcia <92758755+BravoAlpha2@users.noreply.github.com>
2026-04-21 22:05:55 +0100

    Add files via upload

---
+++ run_tests.R
@@ -0,0 +1,16 @@
+
+# Author: Nuno Garcia
+# Date: 21/04/2026
+# LinkedIn: https://www.linkedin.com/in/nuno-garcia-97b780158/
+# ORCID: https://orcid.org/0000-0001-7917-3286
+#
+# Script purpose:
+# Run unit tests for the application.
+
+# Shiny app (main):
+# Dashboard to explore biodiversity observations in Poland.
+# Users can search species by vernacularName and scientificName,
+# inspect observations on a map, and view a temporal timeline.
+
+library(testthat)
+test_dir("tests/testthat")
